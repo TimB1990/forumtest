67eb361c4678340c11beec1e1c275d27a00f0864
TimB1990 <t.boons@live.nl>
2020-12-25 13:48:25 +0100

    first

---
+++ resources/js/components/App.vue
@@ -0,0 +1,46 @@
+<template>
+   <div class="container">
+      <header class="container__head">This is a header</header>
+      <nav class="container__breadcrumbs">... > ... > ... </nav>
+      <main class="container__main">
+
+         <!-- main cols -->
+         <div class="forums">forums</div>
+         <div class="forum-stats">Statestieken - forum</div>
+         <div class="member-stats">Statistieken - leden</div>
+
+         <!-- side cols -->
+         <div class="recent-subjects">Recent subjects</div>
+         <div class="recent-status-updates">Recent status updates</div>
+         <div class="recent-online">Recente gebruikers online</div>
+
+      </main>
+      <footer class="container__foot">
+         <div>Here go social links</div>
+         <div>
+            <span>Taal</span>
+            <span>Privacy Policy</span>
+            <span>Contact</span>
+            <span>Regels</span>
+         </div>
+         <div>
+            Testform &copy; 2021
+         </div>
+      </footer>
+   </div>
+
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         message: "I am saying Hello",
+      };
+   },
+
+   mounted() {
+      console.log("Example Component Mounted");
+   },
+};
+</script>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
